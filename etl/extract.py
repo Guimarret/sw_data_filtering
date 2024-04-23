d24791b3bacbd3aa5a6c75ceba2c456fb8cabf45
Guimarret <guimarret@gmail.com>
2024-04-23 19:03:27 -0300

    initial code version

--- etl/extract.py
+++ etl/extract.py
@@ -1,38 +1,10 @@
-import httpx
-import time
-import json
-import pandas as pd
-import io
-import re
+from utils import process_item, get_data
+import logging
 
-bucket = "bucket-medallion"
-api_url = "https://swapi.dev/api/planets"
-
-# req = httpx.get(api).json()
-# raw = req.json()
-
-init_df = []
-i = 1
-while i < 10:
-    api_url = f"https://swapi.dev/api/planets?page={i}"
-    req = httpx.get(api_url).json()
+def lambda_handler(event, context):
+    api_url = ["planets","films","people","people","starships","vehicles"]
     try:
-        raw = req["results"]
-        init_df.append(raw)
-    except:
-        pass
-    i+=1
-
-df = pd.DataFrame.from_records(init_df)
-
-file_execution_time = time.strftime("%d_%H_%M")
-execution_year = time.strftime("%Y")
-execution_month = time.strftime("%m")
-
-# import boto3
-# try:
-#     s3 = boto3.resource("s3")
-#     s3object = s3.Object(bucket, f"bronze/{execution_year}/{execution_month}/ingest_{file_execution_time}.json")
-# except Exception as err:
-#     logging.error('Exception was thrown in connection %s' % err)
-# s3object.put(Body=(bytes(json.dumps(req.json()).encode("UTF-8"))))
+        get_data(api_url)
+    except Exception as err:
+        logging.error('Exception was thrown in getting data %s' % err)
+    return
---
+++ etl/utils.py
@@ -0,0 +1,40 @@
+import re
+import pandas as pd
+import json
+import httpx
+import logging
+
+def process_item(item):
+    url_numeric_part = re.findall(r'\d+', item['url'])[0]
+    item['url'] = url_numeric_part
+    return item
+
+def get_data(api_urls:list):
+
+    for api in api_urls:
+        init = []
+        i = 1
+        while i < 10:
+            api_url = f"https://swapi.dev/api/{api}?page={i}"
+            req = httpx.get(api_url, verify=False).json()
+            try:
+                raw = req["results"]
+                init.append(raw)
+            except:
+                pass
+            i+=1
+
+        flattened_data = []
+        for sublist in init:
+            for item in sublist:
+                processed_item = process_item(item)
+                flattened_data.append(processed_item)
+        df = pd.DataFrame(flattened_data)
+
+        try:
+            s3_url = f"s3://dev-data-mesh/sw_data_{api}.parquet" 
+            df.to_parquet(s3_url, compression='snappy')
+            logging.info(f"Sucessful ingestion on {s3_url}") 
+        except Exception as err:
+            logging.error('Exception was thrown in connection %s' % err)
+    return
